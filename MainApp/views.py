277f94242437265768740612b55190261144e55b
mazemdevol <fidavam@gmail.com>
2025-07-20 14:52:02 +0200

    lesson 1 end

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,17 +1,35 @@
+from MainApp.models import Snippet
 from django.http import Http404
 from django.shortcuts import render, redirect
 
 
 def index_page(request):
-    context = {'pagename': 'Главное меню'}
+    context = {
+        'pagename': 'Главное меню'
+    }
     return render(request, 'pages/index.html', context)
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
+    context = {
+        'pagename': 'Добавление нового сниппета'
+    }
     return render(request, 'pages/add_snippet.html', context)
 
 
 def snippets_page(request):
-    context = {'pagename': 'Просмотр сниппетов'}
+    snippets = Snippet.objects.all()
+    context = {
+        'pagename': "Список всех сниппетов",
+        'snippets': snippets
+    }
     return render(request, 'pages/view_snippets.html', context)
+
+
+def view_snippet(request, id):
+    snippet = Snippet.objects.get(id=id)
+    context = {
+        'pagename': "Информация о сниппете",
+        'snippet': snippet
+    }
+    return render(request, 'pages/snippet_page.html', context)
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index_page, name='home'),
     path('snippets/add', views.add_snippet_page, name='snippets-add'),
     path('snippets/list', views.snippets_page, name='snippets-list'),
+    path('snippet/<int:id>', views.view_snippet, name="snippet-page"),
 ]
