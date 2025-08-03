69f69cb2848faa8afcdea2f23b4d0fd74d865683
mazemdevol <fidavam@gmail.com>
2025-08-03 16:23:03 +0200

    feat: integrate django-messages into the project

--- MainApp/templatetags/my_tags.py
+++ MainApp/templatetags/my_tags.py
@@ -6,4 +6,12 @@ register = template.Library()
 def get_lang_icon(lang):
     return LANG_ICONS.get(lang)
 
+def messages_type_mapping(value):
+    mapping = {
+        'error' : 'danger',
+        'debug' : 'dark'
+    }
+    return mapping.get(value, value)
+
 register.filter('get_lang_icon',get_lang_icon)
+register.filter('messages_type_mapping', messages_type_mapping)
--- MainApp/views.py
+++ MainApp/views.py
@@ -5,7 +5,7 @@ from django.db.models import F, Q, Count, Avg
 from django.shortcuts import render, redirect, get_object_or_404
 from MainApp.forms import SnippetForm, UserRegistrationForm, CommentForm
 from MainApp.models import LANG_ICONS, LANG_CHOICES, ACCESS_CHOICES
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.contrib.auth.models import User
@@ -216,7 +216,8 @@ def user_registration(request):
     if request.method == "POST":
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            user_form.save()
+            user = user_form.save()
+            messages.success(request, f'Добро пожаловать, {user.username}! Вы успешно зарегистрированы.')
             return redirect("home")
         else:
             context = {
